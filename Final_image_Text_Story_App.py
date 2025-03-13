68c4cdbd5a470088dd2bc3046d69a94d04dcfb42
VaishnaviV0 <vaishnavivishwakarma003@gmail.com>
2025-03-13 15:21:40 +0530

    Update Final_image_Text_Story_App.py

--- Final_image_Text_Story_App.py
+++ Final_image_Text_Story_App.py
@@ -64,9 +64,9 @@ def generate_story_from_text(scenario: str) -> str:
 def main() -> None:
     st.set_page_config(page_title="IMAGE TO STORY CONVERTER", page_icon="🖼️")
     st.markdown(css_code, unsafe_allow_html=True)
-    st.image("PragyanAI_Transperent_github.png")
+    st.image("img.jpg")
     with st.sidebar:
-        st.image("PragyanAI_Transperent_github.png")
+        st.image("img.jpg")
         st.write("AI App created by @ PragyanAI - Education Purpose")
         st.write("Contact Sateesh Ambesange for 5 Days Workshop:pragyan.ai.school@gmail.com")
         st.write("TB - Story to Audio will be done Later")
